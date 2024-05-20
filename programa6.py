76183a03eb3db62615f60707b27eca9bbb808106
Ernestoolono <jose_3141230016@utd.edu.mx>
2024-05-20 11:50:21 -0600

    Programa 6 - Número Mayor

---
+++ programa6.py
@@ -0,0 +1,13 @@
+Numero1 = int(input("Ingresa el primer numero: "))
+
+Numero2 = int(input("Ingresa el segundo numero: "))
+
+if Numero1 > Numero2:
+    print("Numero 1 es mayor que el Numero 2")
+
+elif Numero1 == Numero2:
+    print("Numero 1 y Numero 2 son iguales")
+
+else:
+    print("El Numero 1 es menor que el Numero 2")
+    
