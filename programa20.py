c9265303e4f4993200e7dfd6f98059ebf569a56e
Ernestoolono <jose_3141230016@utd.edu.mx>
2024-06-03 22:37:02 -0600

    programa20 -Conjunto Numeros pares

---
+++ programa20.py
@@ -0,0 +1,11 @@
+conjunto1 = {1, 2, 3, 4, 5, 6, 7}
+conjunto2 = {2, 8, 9, 10, 11, 12, 3, 4, 5}
+conjunto3 = {5, 6, 7, 13, 14, 15, 16, 2}
+
+union = conjunto1.union(conjunto2, conjunto3)
+
+interseccion = conjunto1.intersection(conjunto2, conjunto3)
+
+print(union)
+
+print(interseccion)
