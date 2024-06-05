4d000ddbe3b0a9ed97bb097d1a9107e68de070e6
Ernestoolono <jose_3141230016@utd.edu.mx>
2024-06-05 08:02:46 -0600

    Programa 20 - Conjuntos Numeros Pares

--- programa20.py
+++ programa20.py
@@ -2,10 +2,10 @@ conjunto1 = {1, 2, 3, 4, 5, 6, 7}
 conjunto2 = {2, 8, 9, 10, 11, 12, 3, 4, 5}
 conjunto3 = {5, 6, 7, 13, 14, 15, 16, 2}
 
-union = conjunto1.union(conjunto2, conjunto3)
+union = conjunto1 | conjunto2 | conjunto3
 
-interseccion = conjunto1.intersection(conjunto2, conjunto3)
+conjunto_pares = {valor for valor in union if valor % 2 == 0}
 
 print(union)
 
-print(interseccion)
+print(conjunto_pares)
