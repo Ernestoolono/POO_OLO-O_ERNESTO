f1cf062741724b972adbb3fd518dc9ca7ba89ea9
Ernestoolono <jose_3141230016@utd.edu.mx>
2024-06-05 08:00:23 -0600

    Programa 10 - Suma

---
+++ programa10.py
@@ -0,0 +1,10 @@
+while True:
+    num1 = float(input("Ingrese el primer numero: "))
+    num2 = float(input("Ingrese el segundo numero: "))
+    Suma = num1 + num2
+    print(Suma)
+
+    if input("Precione N para salir") == "N":
+
+        break
+    
