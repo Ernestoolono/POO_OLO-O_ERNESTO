c65a1f5d5a29cdb50e15773d87dd7f477d1f723c
Ernestoolono <jose_3141230016@utd.edu.mx>
2024-06-24 11:18:29 -0600

    programa 23 - Clases y Objetos

---
+++ programa23.py
@@ -0,0 +1,33 @@
+class Coche:
+    def __init__(self, marca, modelo, año):
+        self.marca = marca
+        self.modelo = modelo
+        self.año = año
+        self.en_marcha = False
+    
+    def arrancar(self):
+        self.en_marcha = True
+        print("El coche", self.marca, self.modelo, "ha arrancado.")
+    
+    def detener(self):
+        self.en_marcha = False
+        print("El coche", self.marca, self.modelo, "se ha detenido.")
+    
+    def mostrar_info(self):
+        print("Marca:", self.marca, "Modelo:", self.modelo, "Año:", self.año, "En marcha:", 'Sí' if self.en_marcha else 'No',)
+
+# Crear dos objetos a partir de la clase Coche
+coche1 = Coche("Toyota", "Corolla", 2020)
+coche2 = Coche("Honda", "Civic", 2018)
+
+# Usar algunos atributos y métodos
+coche1.mostrar_info()
+coche1.arrancar()
+coche1.detener()
+
+
+
+coche2.mostrar_info()
+coche2.arrancar()
+coche2.detener()
+
